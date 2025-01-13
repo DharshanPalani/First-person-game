d7e6bbdbf907539ec0202c38c56f353855fec992
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-01-13 19:38:13 +0530

    Added gun reload system with ammo

--- Assets/_Project/Script/GunScript/GunScript.cs
+++ Assets/_Project/Script/GunScript/GunScript.cs
@@ -8,10 +8,8 @@ public class GunScript : MonoBehaviour
     private float _damage;
     private float _range;
     private float _cooldownTime;
-    private float _currentMagazine;
     private float _currentClip;
     private float _currentAmmo;
-    private float _maxMagazine;
     private float _maxClip;
     private float _maxAmmo;
 
@@ -25,6 +23,7 @@ public class GunScript : MonoBehaviour
     [SerializeField] private float autoAimStrength = 5f;
     [SerializeField] private float redDotDuration = 0.5f;
 
+    private GameObject _currentRedDot;
     private float _nextFireTime;
 
     private void Start()
@@ -32,11 +31,11 @@ public class GunScript : MonoBehaviour
         _damage = _currentGun.damage;
         _range = _currentGun.range;
         _cooldownTime = _currentGun.coolDown;
-        _maxMagazine = _currentGun.magazineSize;
         _maxClip = _currentGun.clipSize;
+        _maxAmmo = _currentGun.ammoSize;
 
-        _currentMagazine = _maxMagazine;
         _currentClip = _maxClip;
+        _currentAmmo = _maxAmmo;
     }
 
     void Update()
@@ -65,11 +64,7 @@ public class GunScript : MonoBehaviour
             if (hit.transform.GetComponent<Health>() is { entityType: Health.EntityType.Enemy })
             {
                 // Place a red dot on the enemy
-                GunRedDot gunRedDot = GetComponent<GunRedDot>();
-                if (gunRedDot != null)
-                {
-                    gunRedDot.PlaceRedDot(hit);
-                }
+                PlaceRedDot(hit);
 
                 // Auto-aim: Slightly adjust aim toward the enemy
                 Vector3 targetDirection = (hit.point - cameraPosition).normalized;
@@ -80,15 +75,50 @@ public class GunScript : MonoBehaviour
         }
     }
 
-    
+    private void PlaceRedDot(RaycastHit hit)
+    {
+        float offsetDistance = 0.2f;
+
+        if (_currentRedDot == null)
+        {
+            _currentRedDot = new GameObject("RedDot");
+            var spriteRenderer = _currentRedDot.AddComponent<SpriteRenderer>();
+
+            Texture2D texture = new Texture2D(16, 16);
+            Color[] pixels = new Color[16 * 16];
+            for (int i = 0; i < pixels.Length; i++) pixels[i] = Color.red;
+            texture.SetPixels(pixels);
+            texture.Apply();
+
+            Sprite redDotSprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
+            spriteRenderer.sprite = redDotSprite;
+
+            _currentRedDot.transform.localScale = Vector3.one * 1f;
+        }
+
+        float distanceToHit = Vector3.Distance(_raycastCamera.transform.position, hit.point);
+
+        if (distanceToHit <= _range)
+        {
+            Vector3 offsetPosition = hit.point + hit.normal * offsetDistance;
+            _currentRedDot.transform.position = offsetPosition;
+            _currentRedDot.SetActive(true);
+        }
+        else
+        {
+            _currentRedDot.SetActive(false);
+        }
+
+        Destroy(_currentRedDot, redDotDuration);
+    }
 
 
     private void Shoot()
     {
         _nextFireTime = Time.time + _cooldownTime;
-        if (_currentMagazine == 0) return;
+        if (_currentClip <= 0) return;
 
-        _currentMagazine -= 1;
+        _currentClip -= 1;
 
         cameraShakeEvent.Invoke();
         FindObjectOfType<AudioManager>().Play("AK-47");
@@ -114,5 +144,9 @@ public class GunScript : MonoBehaviour
     private void Reload()
     {
         float reloadAmt = _maxClip - _currentClip;
+        reloadAmt = (_currentAmmo - reloadAmt) >= 0 ? reloadAmt : _currentAmmo;
+
+        _currentClip += reloadAmt;
+        _currentAmmo -= reloadAmt;
     }
 }
--- Assets/_Project/Script/GunScript/GunScriptableObject.cs
+++ Assets/_Project/Script/GunScript/GunScriptableObject.cs
@@ -11,6 +11,5 @@ public class GunScriptableObject : ScriptableObject
     public float coolDown;
 
     public float clipSize;
-    public float magazineSize;
     public float ammoSize;
 }
